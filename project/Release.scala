46caf4b6e47053ac02b3a07755ab7669b0fe4f3a
Tom Heslin <Thomas.Heslin@sky.uk>
2018-10-04 16:57:14 +0100

    Bumping version to 1.0.0

--- project/Release.scala
+++ project/Release.scala
@@ -5,9 +5,6 @@ import sbtrelease.ReleaseStateTransformations._
 import sbtrelease._
 
 object Release {
-  // Useful tasks to show what versions would be used if a release was performed.
-  private val showReleaseVersion = taskKey[String]("the future version once releaseNextVersion has been applied to it")
-  private val showNextVersion = taskKey[String]("the future version once releaseNextVersion has been applied to it")
 
   lazy val releaseSettings = Seq(
     releaseUseGlobalVersion := false,
@@ -27,8 +24,6 @@ object Release {
       setNextVersion,
       commitNextVersion,
       pushChanges
-    ),
-    showReleaseVersion := { val rV = releaseVersion.value.apply(version.value); println(rV); rV },
-    showNextVersion := { val nV = releaseNextVersion.value.apply(version.value); println(nV); nV }
+    )
   )
 }
