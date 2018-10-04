e17d2d2303282f55687bf11fd0332abd68fb1bf2
Tom Heslin <Thomas.Heslin@sky.uk>
2018-10-04 17:03:34 +0100

    Using default sbt-release config

--- project/Release.scala
+++ project/Release.scala
@@ -1,8 +1,5 @@
 import sbt.Keys._
-import sbt.taskKey
 import sbtrelease.ReleasePlugin.autoImport._
-import sbtrelease.ReleaseStateTransformations._
-import sbtrelease._
 
 object Release {
 
@@ -10,20 +7,5 @@ object Release {
     releaseUseGlobalVersion := false,
     releaseVersionBump := sbtrelease.Version.Bump.Minor,
     releaseTagName := s"${name.value}-${version.value}",
-    releaseTagComment := s"Releasing ${version.value} of module: ${name.value}",
-    releaseProcess := Seq[ReleaseStep](
-      checkSnapshotDependencies,
-      releaseStepCommand(ExtraReleaseCommands.initialVcsChecksCommand),
-      inquireVersions,
-      setReleaseVersion,
-      runClean,
-      runTest,
-      commitReleaseVersion,
-      tagRelease,
-      ReleaseStep(releaseStepTask(publish)),
-      setNextVersion,
-      commitNextVersion,
-      pushChanges
-    )
-  )
+    releaseTagComment := s"Releasing ${version.value} of module: ${name.value}")
 }
