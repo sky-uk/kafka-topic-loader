1cd7cd1c8d3a9916ba67ae01b976a54ab81c8449
Ben Carter <benjohn.carter@gmail.com>
2022-01-11 11:38:47 +0000

    build: make versions private

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,40 +2,38 @@ import sbt._
 
 object Dependencies {
 
-  val KafkaVersion = "2.8.0"
-
   object Akka {
-    val akka          = "2.6.16"
-    val stream        = "com.typesafe.akka" %% "akka-stream"         % akka
-    val streamKafka   = "com.typesafe.akka" %% "akka-stream-kafka"   % "2.1.1"
-    val streamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % akka % Test
-    val testkit       = "com.typesafe.akka" %% "akka-testkit"        % akka % Test
-    val base          = Seq(stream, streamKafka)
-    val test          = Seq(streamTestkit, testkit)
+    private val version = "2.6.16"
+    val stream          = "com.typesafe.akka" %% "akka-stream"         % version
+    val streamKafka     = "com.typesafe.akka" %% "akka-stream-kafka"   % "2.1.1"
+    val streamTestkit   = "com.typesafe.akka" %% "akka-stream-testkit" % version % Test
+    val testkit         = "com.typesafe.akka" %% "akka-testkit"        % version % Test
+    val base            = Seq(stream, streamKafka)
+    val test            = Seq(streamTestkit, testkit)
   }
 
   object Cats {
-    val cats   = "2.6.1"
-    val core   = "org.typelevel" %% "cats-core"   % cats
-    val kernal = "org.typelevel" %% "cats-kernel" % cats
-    val all    = Seq(core, kernal)
+    private val version = "2.6.1"
+    val core            = "org.typelevel" %% "cats-core"   % version
+    val kernal          = "org.typelevel" %% "cats-kernel" % version
+    val all             = Seq(core, kernal)
   }
 
   object Refined {
-    val refined    = "0.9.27"
-    val base       = "eu.timepit" %% "refined"            % refined
-    val pureconfig = "eu.timepit" %% "refined-pureconfig" % refined
-    val all        = Seq(base, pureconfig)
+    private val version = "0.9.27"
+    val base            = "eu.timepit" %% "refined"            % version
+    val pureconfig      = "eu.timepit" %% "refined-pureconfig" % version
+    val all             = Seq(base, pureconfig)
   }
 
-  val kafkaClients          = "org.apache.kafka"            % "kafka-clients"           % KafkaVersion
+  val kafkaClients          = "org.apache.kafka"            % "kafka-clients"           % "2.8.0"
   val scalaLogging          = "com.typesafe.scala-logging" %% "scala-logging"           % "3.9.4"
   val logbackClassic        = "ch.qos.logback"              % "logback-classic"         % "1.2.8" % Runtime
   val pureconfig            = "com.github.pureconfig"      %% "pureconfig"              % "0.17.0"
   val scalaCollectionCompat = "org.scala-lang.modules"     %% "scala-collection-compat" % "2.5.0"
 
-  val embeddedKafka = "io.github.embeddedkafka" %% "embedded-kafka" % KafkaVersion % Test
-  val scalaTest     = "org.scalatest"           %% "scalatest"      % "3.2.9"      % Test
+  val embeddedKafka = "io.github.embeddedkafka" %% "embedded-kafka" % "2.8.0" % Test
+  val scalaTest     = "org.scalatest"           %% "scalatest"      % "3.2.9" % Test
 
   val core = Akka.base ++ Cats.all ++ Refined.all ++ Seq(
     kafkaClients,
