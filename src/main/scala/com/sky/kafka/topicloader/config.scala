fd336d706fb366f3d466d3b8ea4ca6e3c54c7139
Ben Carter <benjohn.carter@gmail.com>
2022-01-30 23:10:15 +0000

    load with refined-cats

--- src/main/scala/com/sky/kafka/topicloader/config.scala
+++ src/main/scala/com/sky/kafka/topicloader/config.scala
@@ -1,9 +1,15 @@
 package com.sky.kafka.topicloader
 
+import cats.data.Validated.{Invalid, Valid}
+import cats.implicits._
+import com.typesafe.config.ConfigFactory
+import eu.timepit.refined.cats.syntax._
+import eu.timepit.refined._
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.auto._
-import eu.timepit.refined.numeric.Positive
+import eu.timepit.refined.numeric._
 
+import scala.compat.java8.DurationConverters._
 import scala.concurrent.duration.FiniteDuration
 
 final case class Config(topicLoader: TopicLoaderConfig)
@@ -16,6 +22,24 @@ final case class Config(topicLoader: TopicLoaderConfig)
 final case class TopicLoaderConfig(
     idleTimeout: FiniteDuration,
     bufferSize: Int Refined Positive,
-//    @deprecated("Kept for backward compatibility until clients can adapt", "TopicLoader 1.3.0")
+    @deprecated("Kept for backward compatibility until clients can adapt", "TopicLoader 1.3.0")
     parallelism: Int Refined Positive = 1
 )
+
+object Config {
+  def load(): Config = {
+    val basePath     = "topic-loader"
+    val loadedConfig = ConfigFactory.load()
+    (
+      Valid(loadedConfig.getDuration(s"$basePath.idle-timeout").toScala),
+      refineV[Positive](loadedConfig.getInt(s"$basePath.buffer-size")).toValidatedNec,
+      refineV[Positive](loadedConfig.getInt(s"$basePath.parallelism")).toValidatedNec
+    )
+      .mapN(TopicLoaderConfig.apply) match {
+      case Valid(topicLoaderConfig) => Config(topicLoaderConfig)
+      case Invalid(e)               => throw ConfigLoadException(e.show)
+    }
+  }
+}
+
+final case class ConfigLoadException(message: String) extends RuntimeException
