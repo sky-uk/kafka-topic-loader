91f7e96ee773962d9ec6ba3e897eda24b2c3db3f
Ben Carter <benjohn.carter@gmail.com>
2022-02-21 18:54:15 +0000

    move classes and traits out of package object

--- src/main/scala/com/sky/kafka/topicloader/config/config.scala
+++ src/main/scala/com/sky/kafka/topicloader/config/config.scala
@@ -1,14 +1,10 @@
 package com.sky.kafka.topicloader
 
-import java.time.Duration
-import java.util.concurrent.TimeUnit
-
 import cats.data.ValidatedNec
 import cats.implicits._
-import com.typesafe.config.{Config => TypesafeConfig, ConfigException}
+import com.typesafe.config.{Config => TypesafeConfig}
 
-import scala.concurrent.duration.FiniteDuration
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 package object config {
   type ValidationResult[A] = ValidatedNec[Throwable, A]
@@ -17,40 +13,7 @@ package object config {
     def validate(): ValidationResult[A] = t.toEither.toValidatedNec
   }
 
-  final case class PosInt(private val _value: Int) {
-    val value: Int = _value
-  }
-
-  object PosInt {
-    def apply(value: Int): PosInt =
-      if (value > 0) new PosInt(value)
-      else throw new IllegalArgumentException(s"$value is not a positive Int")
-  }
-
-  trait FromConfig[A] {
-    def fromConfig(path: String, config: TypesafeConfig): A
-  }
-
-  object FromConfig {
-    implicit val intReader: FromConfig[Int]                       = (path: String, config: TypesafeConfig) => config.getInt(path)
-    implicit val posIntReader: FromConfig[PosInt]                 = (path: String, config: TypesafeConfig) =>
-      Try(PosInt(intReader.fromConfig(path, config))) match {
-        case Failure(exception) =>
-          exception match {
-            case e: IllegalArgumentException => throw new ConfigException.BadValue(path, e.getMessage)
-            case _                           => throw exception
-          }
-        case Success(value)     => value
-      }
-    implicit val durationReader: FromConfig[Duration]             = (path: String, config: TypesafeConfig) =>
-      config.getDuration(path)
-    implicit val finiteDurationReader: FromConfig[FiniteDuration] = (path: String, config: TypesafeConfig) =>
-      FiniteDuration(durationReader.fromConfig(path, config).toNanos, TimeUnit.NANOSECONDS)
-  }
-
   implicit class RichConfig(private val underlying: TypesafeConfig) extends AnyVal {
-
     def get[A : FromConfig](path: String): A = implicitly[FromConfig[A]].fromConfig(path, underlying)
-
   }
 }
--- src/main/scala/com/sky/kafka/topicloader/config/topicLoaderConfig.scala
+++ src/main/scala/com/sky/kafka/topicloader/config/topicLoaderConfig.scala
@@ -1,11 +1,44 @@
 package com.sky.kafka.topicloader.config
 
+import java.time.Duration
+import java.util.concurrent.TimeUnit
+
 import cats.data.Validated
 import cats.implicits._
 import com.typesafe.config.{Config => TypesafeConfig, ConfigException}
 
 import scala.concurrent.duration.FiniteDuration
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
+
+trait FromConfig[A] {
+  def fromConfig(path: String, config: TypesafeConfig): A
+}
+
+object FromConfig {
+  implicit val intReader: FromConfig[Int]                       = (path: String, config: TypesafeConfig) => config.getInt(path)
+  implicit val posIntReader: FromConfig[PosInt]                 = (path: String, config: TypesafeConfig) =>
+    Try(PosInt(intReader.fromConfig(path, config))) match {
+      case Failure(exception) =>
+        exception match {
+          case e: IllegalArgumentException => throw new ConfigException.BadValue(path, e.getMessage)
+          case _                           => throw exception
+        }
+      case Success(value)     => value
+    }
+  implicit val durationReader: FromConfig[Duration]             = (path: String, config: TypesafeConfig) => config.getDuration(path)
+  implicit val finiteDurationReader: FromConfig[FiniteDuration] = (path: String, config: TypesafeConfig) =>
+    FiniteDuration(durationReader.fromConfig(path, config).toNanos, TimeUnit.NANOSECONDS)
+}
+
+final case class PosInt(private val _value: Int) {
+  val value: Int = _value
+}
+
+object PosInt {
+  def apply(value: Int): PosInt =
+    if (value > 0) new PosInt(value)
+    else throw new IllegalArgumentException(s"$value is not a positive Int")
+}
 
 final case class Config(topicLoader: TopicLoaderConfig)
 
