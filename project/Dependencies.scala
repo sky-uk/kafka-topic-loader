2e277e059ec7f9f942153464cd1a9bf72c5d0c76
Scala Steward <disco-scala-steward-github-user@skyglobal.onmicrosoft.com>
2022-04-29 16:28:43 +0000

    Revert commit(s) aeb4a82, 94483f9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   }
 
   object Akka {
-    private val version = "2.6.19"
+    private val version = "2.6.18"
     val stream          = "com.typesafe.akka" %% "akka-stream"         % version
     val streamKafka     = "com.typesafe.akka" %% "akka-stream-kafka"   % "2.1.1"
     val streamTestkit   = "com.typesafe.akka" %% "akka-stream-testkit" % version % Test
