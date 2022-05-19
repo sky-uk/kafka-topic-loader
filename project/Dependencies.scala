28f9ac4729bde8c75f12db3f866531f178d54c05
Ben Carter <benjohn.carter@gmail.com>
2022-05-19 19:31:40 +0100

    Update akka-stream-kafka from 2.1.1 to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   object Akka {
     private val version = "2.6.19"
     val stream          = "com.typesafe.akka" %% "akka-stream"         % version cross CrossVersion.for3Use2_13
-    val streamKafka     = "com.typesafe.akka" %% "akka-stream-kafka"   % "2.1.1" cross CrossVersion.for3Use2_13
+    val streamKafka     = "com.typesafe.akka" %% "akka-stream-kafka"   % "3.0.0" cross CrossVersion.for3Use2_13
     val streamTestkit   = "com.typesafe.akka" %% "akka-stream-testkit" % version % Test cross CrossVersion.for3Use2_13
     val testkit         = "com.typesafe.akka" %% "akka-testkit"        % version % Test cross CrossVersion.for3Use2_13
     val base            = Seq(stream, streamKafka)
