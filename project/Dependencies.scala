cfec0194b15e456adc34b64c36e57ad3046e5bf5
Scala Steward <disco-scala-steward-github-user@skyglobal.onmicrosoft.com>
2023-06-06 09:39:08 +0000

    Update kafka-clients from 3.4.0 to 3.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     val all             = Seq(core, kernal)
   }
 
-  val kafkaClients          = "org.apache.kafka"            % "kafka-clients"           % "3.4.0"
+  val kafkaClients          = "org.apache.kafka"            % "kafka-clients"           % "3.4.1"
   val scalaLogging          = "com.typesafe.scala-logging" %% "scala-logging"           % "3.9.5"
   val logbackClassic        = "ch.qos.logback"              % "logback-classic"         % "1.4.7" % Runtime
   val scalaCollectionCompat = "org.scala-lang.modules"     %% "scala-collection-compat" % "2.10.0"
