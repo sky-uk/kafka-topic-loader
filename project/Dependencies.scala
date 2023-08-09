3cdaeed02f98040658691706b97b0ef2e3600ddb
Scala Steward <disco-scala-steward-github-user@skyglobal.onmicrosoft.com>
2023-08-09 15:57:42 +0000

    Update logback-classic from 1.4.7 to 1.4.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
 
   val kafkaClients          = "org.apache.kafka"            % "kafka-clients"           % "3.4.0"
   val scalaLogging          = "com.typesafe.scala-logging" %% "scala-logging"           % "3.9.5"
-  val logbackClassic        = "ch.qos.logback"              % "logback-classic"         % "1.4.7" % Runtime
+  val logbackClassic        = "ch.qos.logback"              % "logback-classic"         % "1.4.10" % Runtime
   val scalaCollectionCompat = "org.scala-lang.modules"     %% "scala-collection-compat" % "2.11.0"
 
   val embeddedKafka = "io.github.embeddedkafka" %% "embedded-kafka" % "3.4.0.1" % Test cross CrossVersion.for3Use2_13
